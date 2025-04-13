9e68286f477d674cf75f43337c24005fd7ee127d
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2025-04-12 20:48:47 -0500

    feat(HotelBookingSystem): Completed User module - User Service and Controller

--- cs489appsd-hotelbooking/src/main/java/edu/miu/cs/cs489appsd/hotel/controllers/UserController.java
+++ cs489appsd-hotelbooking/src/main/java/edu/miu/cs/cs489appsd/hotel/controllers/UserController.java
@@ -4,7 +4,6 @@ import edu.miu.cs.cs489appsd.hotel.dtos.Response;
 import edu.miu.cs.cs489appsd.hotel.dtos.UserDto;
 import edu.miu.cs.cs489appsd.hotel.services.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
